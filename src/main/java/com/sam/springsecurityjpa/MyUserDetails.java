12f353a0a13918d418837c046e26ec27a779e5fa
Sameer Ranjan <dk2547@gmail.com>
2020-07-13 01:43:47 +0530

    spring security sample

--- src/main/java/com/sam/springsecurityjpa/MyUserDetails.java
+++ src/main/java/com/sam/springsecurityjpa/MyUserDetails.java
@@ -2,51 +2,55 @@ package com.sam.springsecurityjpa;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import com.sam.springsecurityjpa.models.User;
+
 public class MyUserDetails implements UserDetails{
 
 	
 	private String userName;
+	private String password;
+	private boolean active;
+	private List<GrantedAuthority> authorities;
 
-	public MyUserDetails(String userName) {
-		this.userName=userName;
-		
+	public MyUserDetails(User user) {
+		this.userName=user.getUserName();
+		this.password=user.getPassword();
+		this.active=user.isActive();
+		this.authorities=Arrays.stream(user.getRoles().split(","))
+				.map(SimpleGrantedAuthority::new)
+				.collect(Collectors.toList());
 	}
-//	public MyUserDetails() {	
-//	}
-	
+
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
-		// TODO Auto-generated method stub
-		return Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
+		return authorities;
 	}
 
 	@Override
 	public String getPassword() {
-		String password="pass";
-		System.out.println("returning password as "+password);
 		return password;
 	}
 
 	@Override
 	public String getUsername() {
-		// TODO Auto-generated method stub
 		return userName;
 	}
 
 	@Override
 	public boolean isAccountNonExpired() {
-		// TODO Auto-generated method stub
 		return true;
 	}
 
 	@Override
 	public boolean isAccountNonLocked() {
-		// TODO Auto-generated method stub
 		return true;
 	}
 
@@ -59,7 +63,8 @@ public class MyUserDetails implements UserDetails{
 	@Override
 	public boolean isEnabled() {
 		// TODO Auto-generated method stub
-		return true;
+		return active;
 	}
+
 
 }
